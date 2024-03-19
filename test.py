d0dd8be70b540ca73b8fb1d7e9e4215b43123a29
Alex <alex@AlexSchoolLaptop.attlocal.net>
2024-03-19 11:57:37 -0500

    Sample Figure

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ import numpy as np
 
 import matplotlib as mpl
 
-fig, ax = plt.subplots()  # Create a figure containing a single axes.
-ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the axes.
-plt.show()
+fig = plt.figure(figsize=(4, 2), facecolor='lightskyblue',
+                 layout='constrained')
+fig.suptitle('A nice Matplotlib Figure')
+ax = fig.add_subplot()
+ax.set_title('Axes', loc='left', fontstyle='oblique', fontsize='medium')
