f42c0ab94d5fa69179fd8a8ea225bce73d42c854
aKoolKev <kevindong0212@gmail.com>
2024-04-11 12:04:53 -0500

    Testing part() of parser.

--- testParser.py
+++ testParser.py
@@ -1,9 +1,84 @@
 from Token import Parser
 
-parse = Parser("x+1")
+#test 1: ALPHA
+print ("Testing     part")
+equation = "x"
+parse = Parser(equation)
 print(parse.tok.input)
 
 if parse.equ():
-    print ("It works!")
+    print ("works!")
 else: 
-    print ("It failed!")
+    print ("failed!")
+
+print("\n\n")
+
+#test 2: INT
+equation = "100"
+parse = Parser(equation)
+print(parse.tok.input)
+if parse.equ():
+    print ("works!")
+else: 
+    print ("failed!")
+
+print("\n\n")
+
+#test 3: REAL
+equation = "100.31415"
+parse = Parser(equation)
+print(parse.tok.input)
+if parse.equ():
+    print ("works!")
+else: 
+    print ("failed!")
+
+print("\n\n")
+
+
+#test 4: LPAREN equ RPAREN
+equation = "(y^2 + 7)"
+parse = Parser(equation)
+print(parse.tok.input)
+if parse.equ():
+    print ("works!")
+else: 
+    print ("failed!")
+
+#test 4.1: LPAREN equ RPAREN
+equation = "y^2 + 7"
+parse = Parser(equation)
+print(parse.tok.input)
+if parse.equ():
+    print ("works!")
+else: 
+    print ("failed!")
+
+#test 4.1: LPAREN equ RPAREN
+equation = "(y^2 + 7) + 1"
+parse = Parser(equation)
+print(parse.tok.input)
+if parse.equ():
+    print ("works!")
+else: 
+    print ("failed!")
+
+print ("\n\n")
+
+#test 5: ID LPAREN equ RPAREN
+equation = "a(a^2 * 7)"
+parse = Parser(equation)
+print(parse.tok.input)
+if parse.equ():
+    print ("works!")
+else: 
+    print ("failed!")
+
+#test 5: ID LPAREN equ RPAREN
+equation = "a(7)"
+parse = Parser(equation)
+print(parse.tok.input)
+if parse.equ():
+    print ("works!")
+else: 
+    print ("failed!")
