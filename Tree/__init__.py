9335d3d381d90afa0ead6391ff85ac43a7c2573a
aKoolKev <kevindong0212@gmail.com>
2024-04-11 12:13:11 -0500

    Minor fix/change to Tree

--- Tree/__init__.py
+++ Tree/__init__.py
@@ -3,7 +3,7 @@ from enum import Enum
 import numpy as np
 
 class NodeType(Enum):
-	CON		= 0
+	CONST		= 0 
 	VAR		= 1
 	ADD		= 2
 	SUB		= 3
@@ -23,7 +23,7 @@ class Node:
 		self.func = None		# The numpy function to call (for call nodes)
 
 	def __call__(self, *args, **kwargs):
-		if self.type == NodeType.CON:	# 0 Children
+		if self.type == NodeType.CONST:	# 0 Children
 			return self.constant
 		if self.type == NodeType.VAR:	# 0 Children
 			return kwargs[self.var]
@@ -36,7 +36,7 @@ class Node:
 		if self.type == NodeType.DIV:	# Both Children
 			return self.left(*args, **kwargs) / self.right(*args, **kwargs)
 		if self.type == NodeType.EXP_C:	# Both Children
-			return self.left(*args, **kwargs) + self.right(*args, **kwargs)
+			return self.left(*args, **kwargs) ** self.right(*args, **kwargs)
 		if self.type == NodeType.CALL:	# Only Right Child. Func Name stored in parent (aka this node)
 			return eval("np."+self.func+"("+str(self.right(*args, **kwargs))+")")
 		raise Error			# Not a valid Node Type
@@ -45,7 +45,7 @@ print(np.sin(10))
 
 # EXAMPLES ARE BELOW
 
-n = Node(NodeType.CON)
+n = Node(NodeType.CONST)
 n.constant = 5
 print(n(x=10))
 
