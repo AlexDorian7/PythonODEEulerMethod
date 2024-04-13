850c4ad0ee55b4fafa07001227c6f328a909bd9b
aKoolKev <kevindong0212@gmail.com>
2024-04-13 13:42:21 -0500

    Finished testing Parser Class.

--- testParser.py
+++ testParser.py
@@ -1,39 +1,39 @@
 from Token import Parser
 
 #test 1: ALPHA
-print ("Testing     part")
+print ("[TESTING PART]")
 equation = "x"
 parse = Parser(equation)
 print(parse.tok.input)
 
 if parse.equ():
-    print ("works!")
+    print ("works!\n")
 else: 
     print ("failed!")
 
-print("\n\n")
+
 
 #test 2: INT
 equation = "100"
 parse = Parser(equation)
 print(parse.tok.input)
 if parse.equ():
-    print ("works!")
+    print ("works!\n")
 else: 
     print ("failed!")
 
-print("\n\n")
+
 
 #test 3: REAL
 equation = "100.31415"
 parse = Parser(equation)
 print(parse.tok.input)
 if parse.equ():
-    print ("works!")
+    print ("works!\n")
 else: 
     print ("failed!")
 
-print("\n\n")
+
 
 
 #test 4: LPAREN equ RPAREN
@@ -41,44 +41,90 @@ equation = "(y^2 + 7)"
 parse = Parser(equation)
 print(parse.tok.input)
 if parse.equ():
-    print ("works!")
+    print ("works!\n")
 else: 
     print ("failed!")
+
 
 #test 4.1: LPAREN equ RPAREN
 equation = "y^2 + 7"
 parse = Parser(equation)
 print(parse.tok.input)
 if parse.equ():
-    print ("works!")
+    print ("works!\n")
 else: 
     print ("failed!")
 
-#test 4.1: LPAREN equ RPAREN
-equation = "(y^2 + 7) + 1"
-parse = Parser(equation)
-print(parse.tok.input)
-if parse.equ():
-    print ("works!")
-else: 
-    print ("failed!")
-
-print ("\n\n")
 
 #test 5: ID LPAREN equ RPAREN
 equation = "a(a^2 * 7)"
 parse = Parser(equation)
 print(parse.tok.input)
 if parse.equ():
-    print ("works!")
+    print ("works!\n")
 else: 
     print ("failed!")
 
-#test 5: ID LPAREN equ RPAREN
-equation = "a(7)"
+
+
+print("[TESTING FACTOR]")
+
+#test 6: part <b>EXP_CHAR</b> part 
+equation = "a^(y+1)+3a"
 parse = Parser(equation)
 print(parse.tok.input)
 if parse.equ():
-    print ("works!")
+    print ("works!\n")
+else: 
+    print ("failed!")
+
+
+print("[TESTING TERM]")
+
+#test 7: factor <b>MD</b> term
+equation = "a^(y+1)+3a * a(a^2 * 7)"
+parse = Parser(equation)
+print(parse.tok.input)
+if parse.factor():
+    print ("works!\n")
+else: 
+    print ("failed!")
+
+#test 7.1: factor <b>MD</b> term
+equation = "a^(y+1)+3a * a^(y+1)+3a"
+parse = Parser(equation)
+print(parse.tok.input)
+if parse.factor():
+    print ("works!\n")
+else: 
+    print ("failed!")
+
+
+#test 8: factor term
+equation = "a^(y+1)+3a a"
+parse = Parser(equation)
+print(parse.tok.input)
+if parse.factor():
+    print ("works!\n")
+else: 
+    print ("failed!")
+
+
+print('[TESTING EQU]')
+#test 9: term <b>PM</b> equ
+equation = "a^(y+1)+3a * a^(y+1)+3a + a^(y+1)+3a"
+parse = Parser(equation)
+print(parse.tok.input)
+if parse.factor():
+    print ("works!\n")
+else: 
+    print ("failed!")
+
+#test 10: term
+equation = "a^(y+1)+3a * a^(y+1)+3a + a^(y+1)+3a * a^(y+1)+3a"
+parse = Parser(equation)
+print(parse.tok.input)
+if parse.factor():
+    print ("works!\n")
 else: 
     print ("failed!")
